ea4804fbcb2cccd54a0e0f78fbccba503a31d72b
animukherjee2006-tech <animukherjee2006@gmail.com>
2026-04-16 23:11:46 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://ieee-1ewt.onrender.com', 
+  baseURL: 'https://ieee-backend-fp0e.onrender.com/', 
   withCredentials: true, // Sabse important line yehi hai!
 });
 
